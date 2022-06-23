303064efc64b4a363b14f0cd3d9d1518d47dc2e1
Callum McCracken <callum189@gmail.com>
2022-06-23 11:50:28 -0700

    update BiPo fitting file

--- BiPo/exponential_fit.py
+++ BiPo/exponential_fit.py
@@ -6,23 +6,27 @@ for the BiPo experiment.
 from scipy.optimize import curve_fit
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams.update({'font.size': 22})
 
 FIT_POINTS = [
-    # (delay in nanoseconds, counts per sec)
+    # (delay in nanoseconds, time in seconds, counts)
     # these are from a spreadsheet,
     # could read from a csv but no need, it's just a few points
-    (188, 2.333333333),
-    (261, 1.633333333),
-    (319, 1.257142857),
-    (314, 1.419047619),
-    (309, 1.352380952),
-    (573, 1.008333333),
-    (705, 0.9833333333),
-    (441, 1.108333333),
-    (393, 1.1),
-    (261, 2.133333333),
-    (324, 1.666666667),
-    (324, 1.6),
+    (560, 900, 80),
+    (410, 900, 100),
+    (290, 900, 164),
+    (165, 900, 597),
+
+    # (534.5, 900, 80),
+    # (402.5, 900, 100),
+    # (270.5, 900, 164),
+    # (138.5, 900, 597),
+
+    # (34, 30, 229),
+    # (132, 30, 125),
+    # (188, 30, 70),
+    # (261, 30, 64)
 ]
 
 # initial guesses for a, b, lam in a + be^(-t * lam)
@@ -36,20 +40,39 @@ def fit_func(delay_time, constant_offset, y_scale, lambda_):
 
 def main():
     """make a fit, calculate half-life, make a plot"""
-    delays, counts = map(np.array, list(zip(*FIT_POINTS)))
-    fit_params, *_ = curve_fit(fit_func, delays, counts, p0=GUESSES)
-    _, _, lam = fit_params
+    delays, times, counts = map(np.array, list(zip(*FIT_POINTS)))
+    counts_per_sec = counts / times
+    err_in_cps = np.sqrt(counts) / times
+    fit_params, fit_covariances = curve_fit(
+        fit_func, delays, counts_per_sec, p0=GUESSES, sigma=err_in_cps)
+    # underscore on the lambda to avoid overwriting builtin lambda
+    constant_offset, y_scale, lambda_ = fit_params
 
-    half_life = np.log(2)/lam
+    half_life = np.log(2)/lambda_
 
-    print(f"{half_life = } ns")
+    # plot data with error bars
+    plt.figure(figsize=(8, 8))
+    plt.scatter(delays, counts_per_sec)
+    plt.errorbar(
+        delays, counts_per_sec,
+        # xerr=calc_act_uncerts,
+        yerr=err_in_cps,
+        fmt='o', ecolor='k', color='k')
 
     fit_line_delays = np.arange(min(delays), max(delays), 1)
-    fit_line_counts = fit_func(fit_line_delays, *fit_params)
+    fit_line_cps = fit_func(fit_line_delays, *fit_params)
+    fit_param_error = np.sqrt(np.diag(fit_covariances))
+    const_err, scale_err, decay_err = fit_param_error
 
-    # plot
-    plt.scatter(delays, counts)
-    plt.plot(fit_line_delays, fit_line_counts)
+    print(f"The fit: {constant_offset:.2f}+{y_scale:.2f}*exp(-{lambda_:.2f}t)")
+    half_life_low = np.log(2)/(lambda_ + decay_err)
+    half_life_high = np.log(2)/(lambda_ - decay_err)
+    print(f"Calculated half life {half_life:.2f} ns")
+    print(f"±1σ half-life band: [{half_life_low}, {half_life_high}]")
+    plt.xlabel("Delay [ns]")
+    plt.ylabel("Counts per second")
+    plt.plot(fit_line_delays, fit_line_cps, 'g')
+    plt.tight_layout()
     plt.savefig("BiPo.png")
 
 
